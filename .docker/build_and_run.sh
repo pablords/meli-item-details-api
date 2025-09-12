dd3e79deae63e7cafde5872c32ae89e656bf620e
Pablo Ruan dos Santos <pablo.santos@telefonicati.onmicrosoft.com>
2025-09-12 19:59:40 -0300

    chore: rename app

--- .docker/build_and_run.sh
+++ .docker/build_and_run.sh
@@ -3,13 +3,13 @@
 DOCKER_REPO="pablords"
 VERSION=$(./mvnw help:evaluate -Dexpression=project.version -q -DforceStdout)
 CONTEXT="dev"
-APPLICATION_NAME="spring-template"
+APPLICATION_NAME="meli-item-details-api"
 
 echo $VERSION
 
 docker build \
--t $DOCKER_REPO/parking:$VERSION \
---build-arg JAR_FILE=target/parking-$VERSION.jar .
+-t $DOCKER_REPO/$APPLICATION_NAME:$VERSION \
+--build-arg JAR_FILE=target/$APPLICATION_NAME-$VERSION.jar .
 
 
 docker run \
