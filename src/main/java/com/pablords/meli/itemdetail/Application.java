0f32b54555cf1de2474ac6aa155b003a533ab922
Pablo Ruan dos Santos <pablo.santos@telefonicati.onmicrosoft.com>
2025-09-13 13:58:33 -0300

    feat: added ComponentScan to FileProductRepositoryAdapter

--- src/main/java/com/pablords/meli/itemdetail/Application.java
+++ src/main/java/com/pablords/meli/itemdetail/Application.java
@@ -1,9 +1,13 @@
 package com.pablords.meli.itemdetail;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.pablords.meli.itemdetail"})
 public class Application {
 
 	public static void main(String[] args) {
