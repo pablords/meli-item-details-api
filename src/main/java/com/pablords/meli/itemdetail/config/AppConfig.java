b7bc6b4210b205ee30c3c1e99302cdc6273fd08a
Pablo Ruan dos Santos <pablo.santos@telefonicati.onmicrosoft.com>
2025-09-13 13:59:34 -0300

    feat: added config to swagger docs

--- src/main/java/com/pablords/meli/itemdetail/config/AppConfig.java
+++ src/main/java/com/pablords/meli/itemdetail/config/AppConfig.java
@@ -1,17 +1,19 @@
 package com.pablords.meli.itemdetail.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import com.pablords.meli.itemdetail.domain.application.ports.inbound.service.ProductServicePort;
 import com.pablords.meli.itemdetail.domain.ports.outbound.repository.ProductRepositoryPort;
 import com.pablords.meli.itemdetail.domain.application.service.ProductService;
 
 @Configuration
-public class BeanConfiguration {
-  
+public class AppConfig {
+
   @Bean
   ProductServicePort productService(ProductRepositoryPort productRepository) {
     return new ProductService(productRepository);
   }
+
+
 }
---
+++ src/main/java/com/pablords/meli/itemdetail/config/SwaggerConfig.java
@@ -0,0 +1,12 @@
+package com.pablords.meli.itemdetail.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Product Details API", version = "v1"))
+public class SwaggerConfig {
+	
+}
